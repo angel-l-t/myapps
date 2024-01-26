565f57b03c689a924451e9a5cad63d6eb99b788f
Angel Lugo Tehuitzil <90810622+angel-l-t@users.noreply.github.com>
2024-01-26 13:16:22 -0600

    Update main.js

--- text-chat-simplifier/main.js
+++ text-chat-simplifier/main.js
@@ -2,109 +2,32 @@ const originalText = document.getElementById("original-conversation");
 const changeButton = document.getElementById("change-button");
 const outputText = document.getElementById("simplified-conversation");
 
+const mentorButton = document.getElementById("mentors-name");
+const mentorNameInput = document.getElementById("name");
+
 var text = null;
+
+var mentorName = localStorage.getItem("mentor");
+
+    if (mentorName == null) {
+        mentorName = "Mentor"
+    }
+mentorNameInput.defaultValue = mentorName;
 
 changeButton.addEventListener("click", () => {
     // Text in input
     text = originalText.value.trim();
 
-    // Text into array
-    var paragraphs = text.split(/(\[[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9] [0-9][0-9]:[0-9][0-9] [pa]\. m\.\] [^:]*: )/g);
-    
-    //Array for input
-    var newParagraphs = "";
-    
-    // This two variables help eliminating repeated date entries
-    var splitDateAndName;
-    var currentName;
-    
-    var lastTimeStampt;
-    var lastTimeFormat;
-    var lastTime;
-    var repeated;
-
-    paragraphs.forEach((paragraph, i) => {
-        if (/(\[[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9] [0-9][0-9]:[0-9][0-9] [pa]\. m\.\] .*: )/g.test(paragraph)) {
-            splitDateAndName = paragraph.split(/(\[[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9] [0-9][0-9]:[0-9][0-9] [pa]\. m\.\])/g);
-            
-            if (currentName == splitDateAndName[2]) {
-                lastTime = paragraph.substring(12, 17);
-                lastTimeFormat = paragraph.substring(18, 23);
-                repeated = true;
-                paragraphs.splice(i, 1);
-            } else {
-
-                if (repeated == true) {
-                    var text = paragraphs[lastTimeStampt]
-                    var index = 22;
-
-                    if (lastTimeFormat == "p. m." && `${parseInt(lastTime)}` != "12") {
-                        var minutes = lastTime.substring(3);
-                        lastTime = `${parseInt(lastTime) + 12}:${minutes}`;
-                    } else if (lastTimeFormat == "a. m." && `${parseInt(lastTime)}` == "12") {
-                        var minutes = lastTime.substring(3);
-                        lastTime = `00:${minutes}`;
-                    }
-
-                    paragraphs[lastTimeStampt] = text.substring(0, index) + lastTime + text.substring(index + lastTime.length);
-                    repeated = false;
-                }
-                
-                if (paragraph.substring(18, 23) == "p. m.") {
-                    if (paragraph.substring(12, 14) != "12") {
-                        var insert = parseInt(paragraph.substring(12, 14)) + 12;
-                        insert = `${insert}`;
-                        var text = paragraph
-                        var index = 12;
-
-                        paragraph = text.substring(0, index) + insert + text.substring(index + insert.length);
-                    }
-                    lastTimeFormat = "p. m.";
-                } else if (paragraph.substring(18, 23) == "a. m.") {
-                    if (paragraph.substring(12, 14) == "12") {
-                        var minutes = `${paragraph.substring(16, 18)}`;
-                        var insert = `00:${minutes}`;
-
-                        var text = paragraph
-                        var index = 12;
-                        paragraph = text.substring(0, index) + insert + text.substring(index + insert.length);
-                    }
-                    lastTimeFormat = "a. m.";
-                }
-
-                if (paragraph.substring(24, 27) != " : ") {
-                    var insert = "";
-
-                    var text = paragraph
-                    var index = 24;
-                    paragraph = text.substring(0, index)
-                }
-
-                var day = paragraph.substring(1,3);
-                var month = paragraph.substring(4,6);
-
-                var text = paragraph
-                var index = 1;
-                paragraph = text.substring(0, index) + month + text.substring(index + month.length);
-
-                text = paragraph
-                index = 4;
-                paragraph = text.substring(0, index) + day + text.substring(index + day.length);
-
-                paragraphs[i] = "\nWhatsApp " + paragraph.replace("[", "<").replace("]", ">").replace("> : ", "> Me").replace(" a. m.", "").replace(" p. m.", "") + "\n";
-
-                currentName = splitDateAndName[2];
-                lastTimeStampt = i;
-            }
-            
-        }
-    });
-
-    paragraphs.forEach((paragraph) => {
-        newParagraphs += paragraph;
-    });
-
-    outputText.innerHTML = newParagraphs.trim();
+    outputText.innerHTML = text.replaceAll("m.] :", `m.] ${mentorName}:`).trim();
 });
 
-// /(\[[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9] [0-9][0-9]:[0-9][0-9] [pa]\. m\.\] .*: )/g
+mentorButton.addEventListener("click", () => {
+    newName = mentorNameInput.value;
+
+    if (newName.trim() == "") {
+        newName = "Mentor"
+    }
+
+    localStorage.setItem("mentor", newName.trim());
+    location.reload()
+})
